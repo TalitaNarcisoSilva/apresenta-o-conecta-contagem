809f21678d7b82b67db99170f63afed13d9d2f3c
Talita <talitanarciso33@gmail.com>
2026-09-10 22:01:48 -0300

    atualização

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 
 // =========================================
 // CONEXÃO COM O BANCO DE DADOS
@@ -56,36 +56,5 @@ $banco = "conecta_contagem";
             "Erro ao conectar ao banco de dados: "
             . $erro->getMessage()
         );
-    }
-}
-
-*/
-
-// TODO: APAGAR ESSE EMBAIXO:
-
-// Função responsável por criar e retornar a conexão com o banco
-function conectarBanco()
-{
-    // Dados de conexão com o banco
-    $host = "127.0.0.1";
-    $porta = "3307"; // Se necessário, troque para 3306
-    $banco = "conecta_contagem";
-    $usuario = "root";
-    $senha = "";
-
-    try {
-        $pdo = new PDO(
-            "mysql:host=$host;port=$porta;dbname=$banco;charset=utf8mb4",
-            $usuario,
-            $senha
-        );
-
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-        return $pdo;
-
-    } catch (PDOException $e) {
-        die("Erro ao conectar: " . $e->getMessage());
     }
 }
